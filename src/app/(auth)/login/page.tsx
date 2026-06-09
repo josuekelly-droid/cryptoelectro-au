c1b1049dbfc6c7ca8d845d01c26694c6f87af73c
Kelly <josuekellyakplogan4001@gmail.com>
2026-06-09 11:50:20 +0200

    feat: vérification email + resend verification + dashboard message

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -145,11 +145,11 @@ export default function LoginPage() {
             </button>
           </form>
 
-          <div className="relative my-6">
+          {/* <div className="relative my-6">
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-secondary-light" />
             </div>
-          </div>
+          </div> */}
 
         </div>
 
